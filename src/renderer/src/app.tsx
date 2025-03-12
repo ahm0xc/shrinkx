6598e34ba94bd14e097934a13a1ce3416fda94e8
ahm0xc <codingwebsa@gmail.com>
2025-03-13 05:12:15 +0600

    init shadcn

--- src/renderer/src/app.tsx
+++ src/renderer/src/app.tsx
@@ -1,3 +1,9 @@
+import { Button } from './components/button'
+
 export default function App() {
-  return <div>App</div>
+  return (
+    <div className="text-3xl">
+      App <Button>Hello</Button>
+    </div>
+  )
 }
--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/renderer/src/**/*.{ts,tsx}', './src/renderer/index.html'],
-  theme: {
-    extend: {}
-  },
-  plugins: []
-}
