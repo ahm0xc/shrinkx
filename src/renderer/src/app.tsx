c7f28a4f012257a0016959e8e5867dfc383cfc84
ahm0xc <codingwebsa@gmail.com>
2025-03-13 11:09:33 +0600

    UI: shell

--- src/renderer/src/app.tsx
+++ src/renderer/src/app.tsx
@@ -1,9 +1,12 @@
-import { Button } from './components/ui/button'
-
 export default function App() {
   return (
-    <div className="text-3xl">
-      App <Button>Hello</Button>
-    </div>
+    <main className="grid grid-cols-5 h-[calc(100vh-2.5rem)] overflow-hidden">
+      <div className="col-span-3 h-full p-4 pr-2 pt-0">
+        <div className="w-full h-full bg-foreground/5 rounded-xl"></div>
+      </div>
+      <div className="col-span-2 h-full p-4 pl-2 pt-0">
+        <div className="w-full h-full bg-foreground/5 rounded-xl"></div>
+      </div>
+    </main>
   )
 }
