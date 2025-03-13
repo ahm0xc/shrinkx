9aef1bd83726406c3890c3254ef2197fe603faa5
ahm0xc <codingwebsa@gmail.com>
2025-03-13 10:57:58 +0600

    add: theme provider

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -4,9 +4,12 @@ import ReactDOM from 'react-dom/client'
 import App from './app'
 
 import './assets/globals.css'
+import { ThemeProvider } from './components/theme-provider'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider defaultTheme="dark" storageKey="ui-theme">
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 )
